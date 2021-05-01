bd39f95aa47ba3bf0701e0de9da514cc097fceaf
Tomas Kucera <tom4f@seznam.cz>
2021-05-01 21:27:06 +0200

    vercel test

--- config/index.js
+++ config/index.js
@@ -2,11 +2,11 @@ const dev = process.env.NODE_ENV !== 'production'
 
 export const serverPath = dev ? 'http://localhost/lipnonet/rekreace' : 'https://www.frymburk.com/rekreace'
 
-export const photoApiScript = 'pdo_read_foto_lucka.php'
-export const photoFolder = 'fotogalerie_lucka'
-export const photoGalleryLink = `${serverPath}/fotogalerie_lucka.html`
+export const photoApiScript = 'pdo_read_foto_tomas.php'
+export const photoFolder = 'fotogalerie_tomas'
+export const photoGalleryLink = `${serverPath}/fotogalerie_tomas.html`
 
-export const blogApiScript = 'pdo_read_blog.php'
-export const blogApiScriptExt = ''
-
-export const apiPath = dev ? 'http://localhost:3000' : 'https://olca.cz'
+export const blogApiScript = 'pdo_read_blog_tomas.php'
+export const blogApiScriptExt = '_tomas'
+//
+export const apiPath = dev ? 'http://localhost:3000' : 'https://tomas-blog.vercel.app'
