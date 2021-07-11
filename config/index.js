64df10ff29d6039aea0e893f410dc54bca06c457
Tomas Kucera <tom4f@seznam.cz>
2021-07-11 16:35:50 +0200

    imgs path for production added

--- config/index.js
+++ config/index.js
@@ -4,7 +4,7 @@ export const serverPath = dev ? 'http://localhost/lipnonet/rekreace' : 'https://
 
 export const photoApiScript = 'pdo_read_foto_tomas.php'
 export const photoFolder = 'fotogalerie_tomas'
-export const photoGalleryLink = `${serverPath}/fotogalerie_tomas.html`
+export const photoGalleryLink = `${serverPath}/photo-gallery`
 
 export const blogApiScript = 'pdo_read_blog_tomas.php'
 export const blogApiScriptExt = '_tomas'
